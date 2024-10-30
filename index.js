6c7b80397b2673a3c9e4c6a513d0df64e6077016
Hichamelhmaydi <ct1594065@gmail.com>
2024-10-30 17:14:22 +0100

    change js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function cklick (){
-    document.getElementsByClassName=("aa");
-}
+document.addEventListener('DOMContentLoaded' , () => {
+    const add=document.getElementById('add');
+    const task=document.getElementsByClassName('task');
+    add.addEventListener('click' ,() => task.classList.remove('hidden'));
+});
+
